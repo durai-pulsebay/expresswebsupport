0ad5329d0e406c368ed1966cad073da36f54f7a2
Ramki <ram.ramki0202@gmail.com>
2022-07-22 13:23:00 +0530

    login link generated

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -17,12 +17,15 @@ const Navbar = () => {
             />
           </a>
           <div className="flex  lg:order-2">
-            <button
-              type="button"
-              className="text-white lg:block hidden bg-[#144645] hover:bg-[#226766] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-2.5 text-center mr-3 md:mr-0 "
-            >
-              Login
-            </button>
+            <a href="https://support.expressshopifysupport.com/support/login">
+              <button
+                type="button"
+                className="text-white lg:block hidden bg-[#144645] hover:bg-[#226766] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center mr-3 md:mr-0 "
+              >
+                Login
+              </button>{" "}
+            </a>
+
             <button
               data-collapse-toggle="mobile-menu-4"
               type="button"
@@ -95,12 +98,14 @@ const Navbar = () => {
                 </li>
 
                 <li>
-                  <button
-                    type="button"
-                    className="text-white mt-3 lg:hidden bg-[#144645] hover:bg-[#226766] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Login
-                  </button>
+                  <a className="inline-block" href="">
+                    <button
+                      type="button"
+                      className="text-white mt-3 lg:hidden bg-[#144645] hover:bg-[#226766] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Login
+                    </button>
+                  </a>
                 </li>
               </ul>
             </div>
